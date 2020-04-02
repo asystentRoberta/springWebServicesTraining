70c346060d0936a9e3915c7058f99a2e4cf7433b
AsystentRoberta <asystent.roberta@gmail.com>
2020-04-02 15:48:23 +0200

    Initial commit

---
+++ src/main/java/pl/com/bohdziewicz/trainingWebServices/TrainingWebServicesApplication.java
@@ -0,0 +1,13 @@
+package pl.com.bohdziewicz.trainingWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/bohdziewicz/trainingWebServices/TrainingWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.bohdziewicz.trainingWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
