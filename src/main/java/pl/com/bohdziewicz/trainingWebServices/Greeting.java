00d97f5e08bc57d505cd1c0bd31a8fc385143492
AsystentRoberta <asystent.roberta@gmail.com>
2020-04-04 22:56:00 +0200

    Simple HelloWorld with Spring and Web Controller and so on...

---
+++ src/main/java/pl/com/bohdziewicz/trainingWebServices/Greeting.java
@@ -0,0 +1,12 @@
+package pl.com.bohdziewicz.trainingWebServices;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Greeting {
+
+    private long id;
+    private String content;
+}
